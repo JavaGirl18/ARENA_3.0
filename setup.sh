82751baa2e854be33237bd90709b316a48fb5e29
Valencia Cooper <valenciacooper@Valencias-MacBook-Air.local>
2026-05-11 13:09:01 -0400

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# Install Miniconda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b
+# Install Miniconda if not already installed
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -u
 export PATH="$HOME/miniconda3/bin:$PATH"
 source ~/miniconda3/etc/profile.d/conda.sh
 
+# Accept conda terms
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+
 # Create arena-env
 conda create --name arena-env python=3.11 -y
+conda activate arena-env
 
 # Clone ARENA and run install
-git clone https://github.com/yourusername/ARENA_3.0.git
+git clone https://github.com/JavaGirl18/ARENA_3.0.git
 cd ARENA_3.0
-conda activate arena-env
 bash install.sh
